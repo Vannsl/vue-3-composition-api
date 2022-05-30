224254cb4b9773b04dd5603bfaa09460b6e1f35a
Vanessa Otto <boehner.vanessa@gmail.com>
2022-05-30 18:21:19 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <main class="container mx-auto my-20 px-8">
+    <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  </main>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
