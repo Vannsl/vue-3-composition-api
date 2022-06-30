f6c46b54fcbfceec2cc32e89e94686de54ba20b0
Vanessa Otto <boehner.vanessa@gmail.com>
2022-06-30 07:20:10 +0200

    chore: outsources items into data folder

---
+++ src/data/items.ts
@@ -0,0 +1,17 @@
+export default [
+  {
+    id: 0,
+    name: 'Eggs',
+    count: 10,
+  },
+  {
+    id: 1,
+    name: 'Fresh Milk',
+    count: 1,
+  },
+  {
+    id: 2,
+    name: 'Bread',
+    count: 1,
+  },
+]
